43658a45c6a89b8f57a8d7086c2d93171450e598
Spreeyka <defres15@gmail.com>
2019-08-29 22:23:09 +0200

    Setup routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
+import { SectionSalesComponent } from './sections/section-sales/section-sales.component';
+import { SectionOrdersComponent } from './sections/section-orders/section-orders.component';
+import { SectionHealthComponent } from './sections/section-health/section-health.component';
+
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'sales', component: SectionSalesComponent},
+  {path: 'orders', component: SectionOrdersComponent},
+  {path: 'health', component: SectionHealthComponent},
+  //cala sciezka ma sie zgadzac, moze tez byc prefix czyli szuka matching child routes
+  {path: '', redirectTo: '/sales', pathMatch:'full'},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
